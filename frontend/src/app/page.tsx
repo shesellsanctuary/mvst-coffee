02df14e45d16448795d2507e84734e49aa0479c4
emilycalvet <emilycalvet.inf@gmail.com>
2025-03-01 22:08:46 +0100

    frontend: add coffee service connection to backend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,16 @@
-import { Card } from '@/components/Card';
-import { fetchItems } from '@/services/fetchItems';
+import { Card } from "@/components/Card";
+import { AxiosInstance } from "@/services/coffeeService";
 
 export default async function Home() {
-  const items = await fetchItems();
+  const coffees = (await AxiosInstance.get("/coffee", {})).data;
+
+  if (!coffees) return <div>Loading...</div>;
 
   return (
-    <main className='mx-10 mt-12 px-10'>
-      <h1 className='text-3xl mb-10'>You&apos;ve got this! 🚀</h1>
-      {items.map(({ id, title, description }) => (
-        <Card key={id} title={title} description={description} />
+    <main className="mx-10 mt-12 px-10">
+      <h1 className="text-3xl mb-10">You&apos;ve got this! 🚀</h1>
+      {coffees.map(({ id, name, description }) => (
+        <Card key={id} title={name} description={description} />
       ))}
     </main>
   );
